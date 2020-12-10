41f78996080b5f18fa82481abe40e94e6d943348
Yaromir_Svet <67824344+Yaromir-Svetozarovich@users.noreply.github.com>
2020-12-10 17:35:16 +0700

    Создан класс Planet

--- easySpace_main.py
+++ easySpace_main.py
@@ -1,1 +1,12 @@
-
+from tkinter import *
+class Planet():
+    def __init__(self,x, y,r,colormode):
+        self.x = 0
+        self.y =0
+        self.r = 0
+        self.colormode = ''
+        
+        
+Mars = Planet(10, 10, 3, red)        
+root = Tk()
+root.mainloop()
