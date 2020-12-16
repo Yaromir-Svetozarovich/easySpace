90f34e98cc3c7fb24697b2befc6f840b59e6704f
Yaromir_Svetozarovich <svetozar.derendery@yandex.ru>
2020-12-16 23:25:08 +0700

    Вывод окна с информацией о планете
    Создал окно с выводом информации о планете

--- easySpace_main.py
+++ easySpace_main.py
@@ -2,30 +2,30 @@ import tkinter as tk
 import math 
 from random import randint,choice
 
-
+#Выход из программы
 def window_deleted():
     print ('Окно закрыто')
     root.quit() # явное указание на выход из программы
     
 #Подготовка окна
 root = tk.Tk()
-fr = tk.Frame(root, width=1366,height =758)
+fr = tk.Frame(root, width=1366,height =758)#Создание слоя, на котором будет располагаться canvas
 fr.pack(fill = 'both', expand = 1)
-
 root.title("easySpace")
 root.geometry("1366x768")
-root.protocol('WM_DELETE_WINDOW', window_deleted)
+root.protocol('WM_DELETE_WINDOW', window_deleted)#Проверка на выход из программы
 
-canv = tk.Canvas(fr, height = 710,bg='black')
+canv = tk.Canvas(fr, height = 710,bg='black')#Размещение слоя с графикой
 canv.pack(fill = 'both', expand = 1)
 
-fr2 =tk.Frame(root, width = 1366, height = 10, bg ='black')
+fr2 =tk.Frame(root, width = 1366, height = 10, bg ='black')# Создание слоя для размещения кнопок
 fr2.pack(fill = 'both', expand= 1)
+root.resizable(False, False)#Запрет на изменение размера окна
 
-for i in range(2000) :
+for i in range(4000) :
     coord_x = randint(0,1366)# Выборка координаты x
     coord_y = randint(0,768)# Выборка координаты y
-    r = randint(1,3)# Выборка радиуса звезды
+    r = randint(1,2)# Выборка радиуса звезды
     color = choice(['white','#AFDAFC','#FB607F'])
     canv.create_oval(coord_x-r,coord_y-r, coord_x+r, coord_y+r, fill = color ) #Рисует круг, в случайной позиции
         
@@ -35,6 +35,7 @@ class UI():
         self
 #""" Здесь описаны методы отрисовки планет  """    
     #Необходимо переделать расположение планет и метод задания координат планет, для их перемещения
+    """
     def Sun():#Солнце
         canv.create_oval(583,200,783,400, fill = 'orange', outline = 'red' )
 
@@ -62,43 +63,71 @@ class UI():
 
     def Neptun(event):#Нептун
         canv.create_oval(400,100,500,200,  fill = '#4169E1', outline = 'grey')
-    
+    """
 
-#""" Здесь описаны методы отрисовки и логики при нажатии на клавиши  """        
+#""" Здесь описаны методы отрисовки и логики при нажатии на клавиши  """ 
+#
+#
+#Данный метод вызывает окно, с информацией о планете. На вход нужно подать название планеты, а далее программа выведет рисунок планеты и информацию о ней. 
+
+    def NewWindow(event):
+        #Подготовка окна с информацией о планете
+        info = tk.Toplevel(root, width=800,height =600, bg ='black')
+        info.title("Info about planet") 
+        info.geometry('800x600')
+        info.resizable(False,False) #Запрет на изменение размера окна 
+        #info.overrideredirect(True) Удаляет обрамление окна, можно использовать, но хрень
+        Lab = tk.Label(info, bg = 'black',fg = 'white',text = 'Планета', font = 'Arial 25')
+
+        Lab.pack(side = 'top')
+
+
+       
+
+
+# Далее кнопки можно заменить на планеты, т.е вывод окна с инфой о планете при нажатии на планету
     def Card_Mercury():#Кнопка Меркурия
-        #Рисует карточку-кнопку с инфой по Меркурию 
         button_merc = tk.Button(fr2, width = 2, height = 2, bg ='black', fg = 'white',text = 'Меркурий')
-        #Нажатие на кнопку ЛКМ
-        button_merc.bind("<Button-1>",  UI.Mercury )
+        button_merc.bind("<Button-1>",   UI.NewWindow) 
         button_merc.pack(side = 'left',fill = 'x', expand = True)
+
+
     def Card_Venera():#Кнопка Венеры
         button_ven = tk.Button(fr2 , width = 2, height = 2, bg ='black', fg = 'white',text = 'Венера')
-        button_ven.bind("<Button-1>", UI.Venera )
+        i = 2
+        button_ven.bind("<Button-1>",  UI.NewWindow )
         button_ven.pack(side = 'left',fill = 'x', expand = True)
+
     def Card_Eath():#Кнопка Земли
         button_earth = tk.Button(fr2 , width = 2, height = 2, bg ='black', fg = 'white', text = 'Земля')
-        button_earth.bind("<Button-1>", UI.Earth )
+        button_earth.bind("<Button-1>",  UI.NewWindow )
         button_earth.pack(side = 'left',fill = 'x', expand = True)
+
     def Card_Mars():#Кнопка Марса
         button_mars = tk.Button(fr2 , width = 2, height = 2, bg ='black', fg = 'white', text = 'Марс')
-        button_mars.bind("<Button-1>", UI.Mars )
+        button_mars.bind("<Button-1>",  UI.NewWindow )
         button_mars.pack(side = 'left',fill = 'x', expand = True)   
+
     def Card_Yupiter():#Кнопка Юпитера
         button_yup = tk.Button(fr2 , width = 2, height = 2, bg ='black',  fg = 'white',text = 'Юпитер')
-        button_yup.bind("<Button-1>", UI.Yupiter )
-        button_yup.pack(side = 'left',fill = 'x', expand = True)  
+        button_yup.bind("<Button-1>",  UI.NewWindow )
+        button_yup.pack(side = 'left',fill = 'x', expand = True) 
+
     def Card_Saturn():#Кнопка Юпитера
         button_sat = tk.Button(fr2 , width = 2, height = 2, bg ='black', fg = 'white', text = 'Сатурн')
-        button_sat.bind("<Button-1>", UI.Saturn )
-        button_sat.pack(side = 'left',fill = 'x', expand = True)     
+        button_sat.bind("<Button-1>",  UI.NewWindow )
+        button_sat.pack(side = 'left',fill = 'x', expand = True)  
+
     def Card_Uran():#Кнопка Урана
         button_ur = tk.Button(fr2 , width = 2, height = 2, bg ='black',  fg = 'white',text = 'Уран')
-        button_ur.bind("<Button-1>", UI.Uran )
+        button_ur.bind("<Button-1>",  UI.NewWindow )
         button_ur.pack(side = 'left',fill = 'x', expand = True) 
+
     def Card_Neptun():#Кнопка Нептуна
         button_nep = tk.Button(fr2 , width = 2, height = 2, bg ='black',  fg = 'white',text = 'Нептун')
-        button_nep.bind("<Button-1>", UI.Neptun )
+        button_nep.bind("<Button-1>", UI.NewWindow)
         button_nep.pack(side = 'left',fill = 'x', expand = True) 
+
 
 
 
@@ -109,8 +138,7 @@ class UI():
 
 #Вызов методов 
 def Play():
-    
-    UI.Sun() 
+   # UI.Sun() 
     UI.Card_Mercury()
     UI.Card_Venera()
     UI.Card_Eath()
